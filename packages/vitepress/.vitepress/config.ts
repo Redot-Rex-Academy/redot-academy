2f1c1abd32f2395f1d514fed07395b48df36b594
n00t <jardar@pm.me>
2025-06-12 15:31:33 +0200

    WIP pseudo working order

--- packages/vitepress/.vitepress/config.ts
+++ packages/vitepress/.vitepress/config.ts
@@ -55,7 +55,7 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'github', link: 'https://github.com/Redot-Engine/redot-academy' },
     ]
   },
 
